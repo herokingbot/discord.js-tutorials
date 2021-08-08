5e902bd685a3a01379254c2be885ef3c82cc7c8c
FiredragonPlayz <74649972+FiredragonPlayz@users.noreply.github.com>
2021-08-08 20:21:24 +0530

    #10 Ticket System | Discord.js v13 Series

---
+++ commands/ticket/ticket-panel.js
@@ -0,0 +1,49 @@
+// i have already coded the ticket system and posted it on my server so join my server and go to js codes
+
+const {
+    Client,
+    Message,
+    MessageEmbed,
+    MessageButton,
+    MessageActionRow
+} = require('discord.js');
+
+module.exports = {
+    name: 'ticket-panel',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async (client, message, args, Discord) => {
+        const embed = new MessageEmbed()
+            .setColor('BLUE')
+            .setAuthor(message.guild.name, message.guild.iconURL({
+                dynamic: true
+            }))
+            .setDescription(
+                "__**How to make a ticket**__\n" +
+
+
+                "> Click on the reaction that relates to your need\n" +
+
+                "> Once the ticket is made you will be able to type in there"
+
+            )
+            .setTitle('Tickets')
+
+
+        const bt = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setCustomId('tic')
+                .setLabel('🎫 Create Ticket!')
+                .setStyle('PRIMARY'),
+            );
+
+        message.channel.send({
+            embeds: [embed],
+            components: [bt]
+        });
+    }
+}
